58ab2659936f7a501943468c0897a69425c9c252
yocosaka <sakayoco55@gmail.com>
2021-02-26 00:11:47 +0900

    Add test for openCurrentTabAndContainer in helpers.js in views

--- src/test/views/helpers.test.js
+++ src/test/views/helpers.test.js
@@ -1,4 +1,4 @@
-import { myCreateElement,generateFormField, generateBtn } from '../../scripts/modules/views/helpers';
+import { myCreateElement, generateFormField, generateBtn, openCurrentTabAndContainer } from '../../scripts/modules/views/helpers';
 
 test('should return new element with the given arguments including tag and class name', () => {
   const element = myCreateElement('div', 'my-test-class');
@@ -22,4 +22,18 @@ test('generate a Button with btn class', () => {
   const button = generateBtn('submit');
   expect(button.nodeName).toBe('BUTTON');
   expect(button.classList.contains('btn')).toBe(true);
+});
+
+test('should add active class to the tab and container of the current project', () => {
+  const testId = '123456789';
+  const project = { id: testId };
+  document.body.innerHTML = `
+    <div class="project-tabs__item" data-id="${testId}">Shopping list</div>
+    <div class="project-container" id="${testId}"></div>
+  `;
+  openCurrentTabAndContainer(project);
+  const tabItem = document.querySelector(`[data-id*="${testId}"]`);
+  expect(tabItem.classList.contains('active')).toBe(true);
+  const container = document.querySelector(`[id*="${testId}"]`);
+  expect(container.classList.contains('active')).toBe(true);
 });
